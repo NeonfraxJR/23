e8c1fa3f1f72ee2325a583650d3848fc5a950361
NeonfraxJR <91573755+NeonfraxJR@users.noreply.github.com>
2021-10-05 12:23:29 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let x = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.setGroup(1)
+    x = 1 * 6
+    x = x + 7
+    x = x / 4
+    radio.sendNumber(x)
+})
